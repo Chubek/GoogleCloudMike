567f2d0380410b8a0218f71bfafb313f86ea4a6c
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-07-29 17:16:15 +0430

    fixed stuff

--- RedditScrape.py
+++ RedditScrape.py
@@ -69,7 +69,7 @@ def scrape_reddit():
     submissions = reddit.subreddit("all").search("tap AND water", time_filter=time_frame)
     print(f"Search Done!")
 
-    pattern = re.compile(r"(?i)(?:\040tap\040.*\040water\040|\040tap\040water\.)")
+    pattern = re.compile(r"(?i)(?:\btap\b.*\bwater\b|\040tap\bwater\.)")
 
     for submission in submissions:
         submission.comments.replace_more(limit=0)
@@ -94,28 +94,28 @@ def scrape_reddit():
 
     for city in cities[1:]:
 
-        pattern_text_city = rf"\b(?=\w){city[0]}|\040{city[0].lower()}\b(?!\w)"
+        pattern_text_city = rf"\b(?=\w)\b{city[0]}\b|\b{city[0].lower()}\b(?!\w)"
         print(f"Searching for city {city[0]}, {city[1]} with pattern {pattern_text_city}")
 
-        pattern_city = re.compile(rf"\b(?=\w){city[0]}|\040{city[0].lower()}\b(?!\w)")
+        pattern_city = re.compile(rf"\b(?=\w)\b{city[0]}\b|\b{city[0].lower()}\b(?!\w)")
 
         for res in results:
 
-            if bool(pattern_city.search(res["query_result"])):
-                print(f"City found! {city[0]} in " + res["query_result"])
+            if bool(pattern_city.search(res["query_result"])) and not bool(pattern.search(res["cities_mentioned"])):
+                print(f"City found! {city[0]}")
                 res["cities_mentioned"] = res["cities_mentioned"] + f"{city[0]}, {city[1]}\n"
 
     for country in countries[1:]:
 
-        pattern_text_country = rf"\b(?=\w){country[0]}|\040{country[0].lower()}\b(?!\w)"
+        pattern_text_country = rf"\b(?=\w)\b{country[0]}\b|\b{country[0].lower()}\b(?!\w)"
         print(f"Searching for country {country[0]} with pattern {pattern_text_country}")
 
-        pattern_country = re.compile(rf"\b(?=\w){country[0]}|\040{country[0].lower()}\b(?!\w)")
+        pattern_country = re.compile(rf"\b(?=\w)\b{country[0]}\b|\b{country[0].lower()}\b(?!\w)")
 
         for res in results:
 
-            if bool(pattern_country.search(res["query_result"])):
-                print(f"Country found! {country[0]} in " + res["query_result"])
+            if bool(pattern_country.search(res["query_result"])) and not bool(pattern_country.search(res["countries_mentioned"])):
+                print(f"Country found! {country[0]}")
                 res["countries_mentioned"] = res["countries_mentioned"] + f"{country[0]}\n"
 
     letters = string.ascii_lowercase
