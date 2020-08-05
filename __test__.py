f77d95bb5c35bb727c49f242b68258ef24af9998
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-08-05 07:10:30 +0430

    test

--- __test__.py
+++ __test__.py
@@ -1,3 +1,16 @@
-g = None
-country = "ff"
-print("" if g is None else country)
+from googleapiclient.discovery import build
+
+
+SEARCH_ENGINE_ID = "006168594918175601863:t8oecxasips"
+API_KEY = "AIzaSyCjuHRi_hJDXGBsGKSO4nTaz5k4EQ4K1WI"
+
+service = build("customsearch", "v1", developerKey=API_KEY)
+
+the_result = service.cse().list(q="tap AND water And Los Angeles", cx=SEARCH_ENGINE_ID).execute()
+
+urls = []
+
+for item in the_result.get("items"):
+    urls.append(item.get("link").replace("www", "old"))
+
+print(len(urls))
