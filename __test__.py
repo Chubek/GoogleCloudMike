b1cc68d33d0a2074705c6ed78c99c7634942a91f
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-08-09 22:30:48 +0430

    new stuff

--- __test__.py
+++ __test__.py
@@ -1,5 +1,39 @@
-from googleapiclient.discovery import build
-import sys
+from google.oauth2.service_account import Credentials
+from google.cloud import bigquery
+import gspread
 
-print(sys.argv[1])
+scopes = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
 
+credentials = Credentials.from_service_account_file(
+        'client_secrets.json',
+        scopes=scopes
+    )
+
+gc = gspread.authorize(credentials)
+
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Eg5cF8BCjwZe7rUu5U8m6qckkbCaZHF-jsbk7whTWfI/")
+
+cities_sheet = sh.worksheet("Cities")
+countries_sheet = sh.worksheet("Countries")
+
+cities = cities_sheet.get_all_values()
+
+client = bigquery.Client.from_service_account_json('client_secrets.json')
+
+
+table = client.get_table("cydtw-site.cities.cities_list")
+
+for city in cities[1:]:
+
+    print(f"{city}")
+
+    query = f"UPDATE `cydtw-site.cities.ttap_water_with_cities`\
+        SET a.post_cities = CONCAT(post_cities, ', {city}')\
+        WHERE REGEXP_CONTAINS(post_content, FORMAT('\b%s\b', {city}))\
+        OR REGEXP_CONTAINS(post_title, FORMAT('\b%s\b', {city}))"
+    query_job = client.query(query)
+
+    print("Done")
