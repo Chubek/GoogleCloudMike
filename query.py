974120d6acd584735328011207b59feacfc0c0d8
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-08-13 22:56:39 +0430

    added x

--- query.py
+++ query.py
@@ -21,6 +21,7 @@ cities_sheet = sh.worksheet("Cities")
 countries_sheet = sh.worksheet("Countries")
 
 cities = cities_sheet.get_all_values()
+countries = countries_sheet.get_all_values()
 
 client = bigquery.Client.from_service_account_json('client_secrets.json')
 
@@ -29,12 +30,14 @@ table = client.get_table("cydtw-site.cities.cities_list")
 
 format_str = lambda cty: cty
 
-for city in cities[3:]:
-    city = city[0]
+for city, country in cities[2:]:
     print(f"{city}")
     query = f"UPDATE `cydtw-site.cities.tap_water_with_cities`\
-        SET post_cities = CONCAT(post_cities, FORMAT(', %s', '{city}'))\
+        SET countries_included = CONCAT(countries_included, FORMAT('%s, ', '{country}')),\
+    cities_included = CONCAT(cities_included, FORMAT('%s, ', '{city}'))\
         WHERE REGEXP_CONTAINS(post_content, '{format_str(city)}')\
         OR REGEXP_CONTAINS(post_title, '{format_str(city)}')"
+    print(query)
     query_job = client.query(query)
-ba    print("Done")
+    print(query_job.num_dml_affected_rows)
+    print("Done")
