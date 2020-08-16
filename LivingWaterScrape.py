2531ab7358d6a14a115c5be32810c2d3b0b5b45b
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-08-16 09:55:34 +0430

    fixed

--- LivingWaterScrape.py
+++ LivingWaterScrape.py
@@ -7,149 +7,169 @@ from googleapiclient.discovery import build
 from lxml.html.soupparser import fromstring
 import re
 import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-index_start_file = open("index_start.txt", "r+")
-done_urls_file = open("done_urls.txt", "r+")
 
-index_start = [line.strip().split(";") for line in index_start_file.readlines()]
-done_urls = [url.strip() for url in done_urls_file.readlines()]
+def scrape_living_water():
+    index_start_file = open("index_start.txt", "r+")
+    done_urls_file = open("done_urls.txt", "r+")
 
-ns = {"re": "http://exslt.org/regular-expressions"}
+    index_start = [line.strip().split(";") for line in index_start_file.readlines()]
+    done_urls = [url.strip() for url in done_urls_file.readlines()]
 
-scopes = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
+    ns = {"re": "http://exslt.org/regular-expressions"}
 
-credentials = Credentials.from_service_account_file(
-    'client_secrets.json',
-    scopes=scopes
-)
+    scopes = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
 
-gc = gspread.authorize(credentials)
+    credentials = Credentials.from_service_account_file(
+        'client_secrets.json',
+        scopes=scopes
+    )
 
-sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Eg5cF8BCjwZe7rUu5U8m6qckkbCaZHF-jsbk7whTWfI/")
+    gc = gspread.authorize(credentials)
 
-cities_sheet = sh.worksheet("Cities")
-countries_sheet = sh.worksheet("Countries")
+    sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Eg5cF8BCjwZe7rUu5U8m6qckkbCaZHF-jsbk7whTWfI/")
 
-cities = cities_sheet.get_all_values()
-countries = countries_sheet.get_all_values()
-client = bigquery.Client.from_service_account_json('client_secrets.json')
-table = client.get_table("cydtw-site.living_water.tap_water_living_water")
-SEARCH_ENGINE_ID = "8038b946b9a62a518"
-API_KEY = "AIzaSyC0Mh-ZCUXyK8z0kdMor8viJRmMLSYo67I"
+    cities_sheet = sh.worksheet("Cities")
+    countries_sheet = sh.worksheet("Countries")
 
-service = build("customsearch", "v1", developerKey=API_KEY)
+    cities = cities_sheet.get_all_values()
+    countries = countries_sheet.get_all_values()
+    client = bigquery.Client.from_service_account_json('client_secrets.json')
+    table = client.get_table("cydtw-site.living_water.tap_water_living_water")
+    SEARCH_ENGINE_ID = "8038b946b9a62a518"
+    API_KEY = "AIzaSyC0Mh-ZCUXyK8z0kdMor8viJRmMLSYo67I"
 
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument('--disable-gpu')
-options.add_argument('headless')
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--remote-debugging-port=9222')
-driver = webdriver.Chrome(options=options)
+    service = build("customsearch", "v1", developerKey=API_KEY)
 
-wrap_num = lambda num: 0 if num == 90 else num
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-gpu')
+    options.add_argument('headless')
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--remote-debugging-port=9222')
+    driver = webdriver.Chrome(options=options)
 
-city_marker = int(index_start[-1][0])
+    wrap_num = lambda num: 0 if num == 90 else num
 
-for city, country in cities[int(index_start[-1][0]):]:
-    urls = []
-    city_marker += 1
-    for i in range(int(index_start[-1][1]), 100, 10):
-        index_start_file.write(f"\n{city_marker};{wrap_num(i)}")
-        print(f"Checking {city_marker};{i}")
-        query = f"(tap AND water) AND ({city} OR {country})"
-        the_result = service.cse().siterestrict().list(q=query,
-                                                       start=i,
-                                                       cx=SEARCH_ENGINE_ID).execute()
+    city_marker = int(index_start[-1][0])
 
-        print("Waiting...")
-        time.sleep(65)
-        print("Wait done...")
+    for city, country in cities[int(index_start[-1][0]):]:
+        urls = []
+        city_marker += 1
+        for i in range(int(index_start[-1][1]), 100, 10):
+            index_start_file.write(f"\n{city_marker};{wrap_num(i)}")
+            print(f"Checking {city_marker};{i}")
+            query = f"(tap AND water) AND ({city} OR {country})"
+            the_result = service.cse().siterestrict().list(q=query,
+                                                           start=i,
+                                                           cx=SEARCH_ENGINE_ID).execute()
 
-        pattern = re.compile(rf"(tap.water).(.*{city})|(.quality|.safety)")
+            print("Waiting...")
+            time.sleep(65)
+            print("Wait done...")
 
-        try:
-            for item in the_result.get("items"):
-                if bool(pattern.search(item.get('link'))) and item.get('link') not in done_urls:
-                    urls.append(item.get('link'))
-        except:
-            continue
+            pattern = re.compile(rf"(tap.water).(.*{city})|(.quality|.safety)")
 
-    print(f"Got {len(urls)} urls")
+            try:
+                for item in the_result.get("items"):
+                    if bool(pattern.search(item.get('link'))) and item.get('link') not in done_urls:
+                        urls.append(item.get('link'))
+            except:
+                continue
 
-    for url in urls:
-        print(f"Getting {url}")
+        print(f"Got {len(urls)} urls")
 
-        done_urls.append(url)
-        done_urls_file.write(f"\n{url}")
+        for url in urls:
+            print(f"Getting {url}")
 
-        driver.get(url)
+            done_urls.append(url)
+            done_urls_file.write(f"\n{url}")
 
-        root = fromstring(driver.page_source)
+            driver.get(url)
 
-        tag_contains_pre = ""
-        tag_contains_p = ""
-        tag_contains_li = ""
-        tag_contains_post = ""
+            root = fromstring(driver.page_source)
 
-        try:
-            element_main_p = root.xpath(
-                r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]', namespaces=ns)
-            element_pre_p = root.xpath(
-                r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/preceding-sibling::p', namespaces=ns)
-            element_fll_p = root.xpath(
-                r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/following-sibling::p', namespaces=ns)
+            tag_contains_pre = ""
+            tag_contains_p = ""
+            tag_contains_li = ""
+            tag_contains_post = ""
 
-            for el in element_main_p:
-                tag_contains_p += f"{el.text}\n\n\n"
-            for el in element_pre_p:
-                tag_contains_pre += f"{el.text}\n\n\n"
-            for el in element_fll_p:
-                tag_contains_post += f"{el.text}\n\n\n"
+            try:
+                element_main_p = root.xpath(
+                    r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]', namespaces=ns)
+                element_pre_p = root.xpath(
+                    r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/preceding-sibling::p',
+                    namespaces=ns)
+                element_fll_p = root.xpath(
+                    r'//p[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/following-sibling::p',
+                    namespaces=ns)
 
-            if tag_contains_p:
-                error = client.insert_rows(table, [(url, tag_contains_pre, tag_contains_p,
-                                                    tag_contains_post,
-                                                    "p", f"(tap AND water) AND ({city} OR {country})")])
-                if not error:
-                    print("Inserted")
+                for el in element_main_p:
+                    tag_contains_p += f"{el.text}\n\n\n"
+                for el in element_pre_p:
+                    tag_contains_pre += f"{el.text}\n\n\n"
+                for el in element_fll_p:
+                    tag_contains_post += f"{el.text}\n\n\n"
 
-        except:
-            print("No p")
+                if tag_contains_p:
+                    error = client.insert_rows(table, [(url, tag_contains_pre, tag_contains_p,
+                                                        tag_contains_post,
+                                                        "p", f"(tap AND water) AND ({city} OR {country})")])
+                    if not error:
+                        print("Inserted")
 
-        try:
-            element_main_li = root.xpath(
-                r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]', namespaces=ns)
-            element_pre_li = root.xpath(
-                r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/preceding-sibling::li', namespaces=ns)
-            element_fll_li = root.xpath(
-                r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/following-sibling::li', namespaces=ns)
+            except:
+                print("No p")
 
-            for el in element_main_li:
-                tag_contains_li += f"{el.text}\n\n\n"
-            for el in element_pre_li:
-                tag_contains_pre += f"{el.text}\n\n\n"
-            for el in element_fll_li:
-                tag_contains_post += f"{el.text}\n\n\n"
+            try:
+                element_main_li = root.xpath(
+                    r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]', namespaces=ns)
+                element_pre_li = root.xpath(
+                    r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/preceding-sibling::li',
+                    namespaces=ns)
+                element_fll_li = root.xpath(
+                    r'//li[re:match(.,"\btap\b.*\bwater\b|\bwater\b.*\btap\b", "i")]/following-sibling::li',
+                    namespaces=ns)
 
-            if tag_contains_li:
-                error = client.insert_rows(table, [(url, tag_contains_pre, tag_contains_li,
-                                                    tag_contains_post,
-                                                    "li", f"(tap AND water) AND ({city} OR {country})")])
-                if not error:
-                    print("inserted")
-        except:
-            print("No li")
+                for el in element_main_li:
+                    tag_contains_li += f"{el.text}\n\n\n"
+                for el in element_pre_li:
+                    tag_contains_pre += f"{el.text}\n\n\n"
+                for el in element_fll_li:
+                    tag_contains_post += f"{el.text}\n\n\n"
 
-        del element_fll_li, element_fll_p, element_main_p, element_main_li, \
-            element_pre_p, element_pre_li, tag_contains_p, tag_contains_li, tag_contains_post, tag_contains_pre, root
+                if tag_contains_li:
+                    error = client.insert_rows(table, [(url, tag_contains_pre, tag_contains_li,
+                                                        tag_contains_post,
+                                                        "li", f"(tap AND water) AND ({city} OR {country})")])
+                    if not error:
+                        print("inserted")
+            except:
+                print("No li")
 
-driver.close()
-driver.quit()
+            del element_fll_li, element_fll_p, element_main_p, element_main_li, \
+                element_pre_p, element_pre_li, tag_contains_p, tag_contains_li, tag_contains_post, tag_contains_pre, root
+
+    driver.close()
+    driver.quit()
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        scrape_living_water()
+
+
+if __name__ == "__main__":
+    port = os.environ["PORT"] or 8080
+    myServer = HTTPServer(('', port), MyHandler)
+    myServer.serve_forever()
